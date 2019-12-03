5278a5592b307d3a59590851a606e5655085f28e
Brano <surfin.zap@gmail.com>
2019-12-03 15:14:23 +0100

    Include base typography

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,6 @@
 		.container
 			div
 				logo
-				h1.title
-					| tota
-				h2.subtitle
-					| tota.sk web app
 				.links
 					btn-primary(link="https://tota.sk", label="test")
 					a(class="button--grey", href="https://github.com/nuxt/nuxt.js") GitHub
@@ -50,25 +46,4 @@ export default {
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
