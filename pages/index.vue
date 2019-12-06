406fb5ea4894ea0610c7d5efeead3bb7a52fb76e
Brano <surfin.zap@gmail.com>
2019-12-06 15:03:49 +0100

    Change import to require
    So that the dev server works.

--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,16 @@ export default {
 	},
 
 	head: {
-		title: 'tota agentura'
+		title: 'tota agentura',
+		meta: [
+			{ name: 'description', content: 'Objavujeme, tvoríme a podporujeme (nielen) rusínsku kultúru. Najnovšie sme vydali knihu básní v rusínskom jazyku pre deti. Sme tota agentura.' },
+			{ name: 'keywords', content: 'rusínska kultúra, občianske združenie, tota agentura' },
+			{ name: 'og:type', content: 'website' },
+			{ name: 'og:image', content: 'https://tota.sk/assets/img/tota-agentura-logotype-1200-737.png' },
+			{ name: 'og:url', content: 'https://tota.sk/' },
+			{ name: 'og:title', content: 'Sme tota agentura' },
+			{ name: 'og:description', content: 'Inšpirovaní odkazom našich predkov objavujeme, tvoríme a podporujeme (nielen) rusínsku kultúru ďalšej generácie. Vydali sme knihu básní v rusínskom jazyku pre deti, spolupracovali sme na festivale pre tisíce ľudí. Sme tota agentura.' },
+		]
 	},
 	loading: false,
 }
