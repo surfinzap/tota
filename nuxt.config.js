a213c278428dd1aec18b2bdd5985910bbffba0c2
Brano <surfin.zap@gmail.com>
2019-12-06 09:32:34 +0100

    Generate routes / sitemap for projects

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 module.exports = {
 	loading: false,
 	mode: 'universal',
@@ -46,13 +48,28 @@ module.exports = {
 		enableAdvancedLogging: true,
 		previewApiKey: 'ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.ew0KICAianRpIjogImE4ZDhmZmJmY2U4ZDRhMzY5MTA1ZDU5OWMzODRhZjU0IiwNCiAgImlhdCI6ICIxNTc0ODYzNzc0IiwNCiAgImV4cCI6ICIxOTIwNDYzNzc0IiwNCiAgInByb2plY3RfaWQiOiAiYmI0YzYzMzNmMzYyMDA0MTlkNTZmMThmMThlMzY3MjUiLA0KICAidmVyIjogIjEuMC4wIiwNCiAgImF1ZCI6ICJwcmV2aWV3LmRlbGl2ZXIua2VudGljb2Nsb3VkLmNvbSINCn0.WDySoPcznGD4rldTxwXOAIYBLs7r5uGPfhnT2q7I_SI',
 		enablePreviewMode: false,
-		// baseUrl: 'https://custom.uri/api/KenticoKontentProxy',
-		// securedApiKey: 'xxx',
 		enableSecuredMode: false
+	},
+	generate: {
+		// temp solution, maybe we'll improve it later
+		routes () {
+			return axios.get('https://deliver.kontent.ai/bb4c6333-f362-0041-9d56-f18f18e36725/items?system.type=project&elements=meta__canonical_url')
+				.then((response) => {
+					return response.data.items.map((project) => {
+						return '/projekt/' + project.elements.meta__canonical_url.value
+					})
+				})
+			}
 	},
 	sitemap: {
 		hostname: 'https://tota.sk',
-		gzip: false
+		gzip: false,
+		defaults: {
+			changefreq: 'monthly',
+			priority: 1,
+			lastmod: new Date(),
+			lastmodrealtime: true
+		}
 	},
 	/*
 	** Build configuration
