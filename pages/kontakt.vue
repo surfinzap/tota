96b2c40d1793c8fd48ffc82bf6ed6aaa26912eb9
Brano <surfin.zap@gmail.com>
2019-12-08 09:45:39 +0100

    Add metadata to 'kontakt'

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -47,6 +47,41 @@
 				p Nie sme platcami DPH.
 </template>
 
+
+<script>
+export default {
+	async fetch ({store, params}) {
+		await store.dispatch('homepage/getHomepage');
+	},
+
+	head () {
+		return {
+			title: 'Kontakt — ' + this.$store.state.homepage.homepage.title,
+			meta: [
+				{ name: 'description', content: this.$store.state.homepage.homepage.meta__description },
+				{ name: 'keywords', content: this.$store.state.homepage.homepage.meta__keywords },
+				// Facebook OpenGraph meta
+				{ name: 'og:type', content: 'website' },
+				{ name: 'og:image', content: this.$store.state.homepage.homepage.meta__image },
+				{ name: 'og:url', content: this.$store.state.homepage.homepage.meta__canonical_url },
+				{ name: 'og:title', content: 'Kontakt — ' + this.$store.state.homepage.homepage.title },
+				{ name: 'og:description', content: this.$store.state.homepage.homepage.meta__description },
+				// Twitter cards;
+				{ name: 'twitter:card', content: 'summary' },
+				{ name: 'twitter:title', content: 'Kontakt — ' + this.$store.state.homepage.homepage.title },
+				{ name: 'twitter:description', content: this.$store.state.homepage.homepage.meta__description },
+				{ name: 'twitter:image', content: this.$store.state.homepage.homepage.meta__image },
+				{ name: 'twitter:image:alt', content: this.$store.state.homepage.homepage.meta__image__description }
+			],
+			link: [
+				{ rel: 'canonical', href: this.$store.state.homepage.homepage.meta__canonical_url}
+			]
+		}
+	}
+}
+</script>
+
+
 <style lang='scss'>
 	.kontakt {
 		margin: 0 $grid-gap-column;
