a20f5588d51c06145468392c70ca6bd15eb55b33
Brano <surfin.zap@gmail.com>
2019-12-08 22:31:02 +0100

    Fix apostrophes to interpolated string

--- models/YoutubeVideo.js
+++ models/YoutubeVideo.js
@@ -4,10 +4,10 @@ export class YoutubeVideo extends ContentItem {
 	constructor() {
 		super({
 			richTextResolver: video =>
-				'<figure>'
-				+ '<iframe src="https://youtube.com/embed/${video.id.value}?rel=0" frameborder="0" allowfullscreen></iframe>'
-				+ '<figcaption>${video.description.value}</figcaption>'
-			+ '</figure>'
+				`<figure>`
+				+ `<iframe src="https://youtube.com/embed/${video.id.value}?rel=0" frameborder="0" allowfullscreen></iframe>`
+				+ `<figcaption>${video.description.value}</figcaption>`
+			+ `</figure>`
 		})
 	}
 }
