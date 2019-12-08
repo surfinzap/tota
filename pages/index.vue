d33bacc41b32394d62a556c0fdf24a65c673525b
Brano <surfin.zap@gmail.com>
2019-12-08 09:45:10 +0100

    Remove unused dependencies

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,7 @@
 
 
 <script>
-import HeaderTota from '~/components/header-tota.vue'
 import Tagline from '~/components/tagline.vue'
-import BtnPrimary from '~/components/btn-primary.vue'
 import ProjectsList from '~/components/projects-list.vue'
 
 export default {
