6722ce21c2f1ba39ae8b87ead9388aeedebe3d39
Brano <surfin.zap@gmail.com>
2019-12-09 10:28:16 +0100

    Add styles for youtube video

--- models/YoutubeVideo.js
+++ models/YoutubeVideo.js
@@ -4,9 +4,9 @@ export class YoutubeVideo extends ContentItem {
 	constructor() {
 		super({
 			richTextResolver: video =>
-				`<figure>`
-				+ `<iframe src="https://youtube.com/embed/${video.id.value}?rel=0" frameborder="0" allowfullscreen></iframe>`
-				+ `<figcaption>${video.description.value}</figcaption>`
+				`<figure class="cp--youtube-video">`
+				+ `<iframe src="https://youtube.com/embed/${video.id.value}?rel=0" frameborder="0" allowfullscreen class="cp--youtube-video__media"></iframe>`
+				+ `<figcaption class="cp--youtube-video__caption">${video.description.value}</figcaption>`
 			+ `</figure>`
 		})
 	}
