4f962b09038a31007370890c1104cc4b9d7a80b0
Brano <surfin.zap@gmail.com>
2019-12-09 17:50:23 +0100

    Add favicon for various brosers/systems

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,9 +11,15 @@ module.exports = {
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ name: 'msapplication-TileColor', content: '#ffffff' },
+			{ name: 'theme-color', content: '#ffffff' },
 		],
 		link: [
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+			{ rel:'apple-touch-icon', sizes:'180x180', href:'/apple-touch-icon.png' },
+			{ rel:'icon', type:'image/png', sizes:'32x32', href:'/favicon-32x32.png' },
+			{ rel:'icon', type:'image/png', sizes:'16x16', href:'/favicon-16x16.png' },
+			{ rel:'manifest', href:'/site.webmanifest' },
+			{ rel:'mask-icon', href:'/safari-pinned-tab.svg', color:'#5bbad5' },
 		]
 	},
 	/*
