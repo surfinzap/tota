3f2a11d7b0bef8587fc229323737a245d42b65fe
Brano <surfin.zap@gmail.com>
2019-12-11 00:29:32 +0100

    Remove extra classes

--- models/BookInfo.js
+++ models/BookInfo.js
@@ -5,41 +5,41 @@ export class BookInfo extends ContentItem {
 		super({
 			richTextResolver: book_info =>
 				`<h2>Informácie o publikácii</h2>`
-			+ `<dl class="cp--book-info">`
-				+ `<dt class="cp--book-info__term">Názov</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.title.value}</dd>`
-				+ `<dt class="cp--book-info__term">Autor</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.author.value}</dd>`
-				+ `<dt class="cp--book-info__term">Ilustrácie</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.illustrator.value}</dd>`
-				+ `<dt class="cp--book-info__term">Jazyková úprava</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.language_editor.value}</dd>`
-				+ `<dt class="cp--book-info__term">Jazyk</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.language.value}</dd>`
-				+ `<dt class="cp--book-info__term">ISBN</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.isbn.value}</dd>`
-				+ `<dt class="cp--book-info__term">Rok vydania</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.date_published.value.getFullYear()}</dd>`
-				+ `<dt class="cp--book-info__term">Vydavateľ</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.publisher.value}</dd>`
-				+ `<dt class="cp--book-info__term">Vydanie</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.book_edition.value}</dd>`
-				+ `<dt class="cp--book-info__term">Väzba</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.book_format.value}</dd>`
-				+ `<dt class="cp--book-info__term">Rozmer</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.dimensions.value}</dd>`
-				+ `<dt class="cp--book-info__term">Hmotnosť</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.weight.value} g</dd>`
-				+ `<dt class="cp--book-info__term">Počet strán</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.number_of_pages.value}</dd>`
-				+ `<dt class="cp--book-info__term">Písmo</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.typeface.value}</dd>`
-				+ `<dt class="cp--book-info__term">Papier</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.paper.value}</dd>`
-				+ `<dt class="cp--book-info__term">Tlač</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.printer.value}</dd>`
-				+ `<dt class="cp--book-info__term">Finančná podpora</dt>`
-				+ `<dd class="cp--book-info__data">${book_info.financial_aid.value}</dd>`
+			+ `<dl class="cp--dl">`
+				+ `<dt>Názov</dt>`
+				+ `<dd>${book_info.title.value}</dd>`
+				+ `<dt>Autor</dt>`
+				+ `<dd>${book_info.author.value}</dd>`
+				+ `<dt>Ilustrácie</dt>`
+				+ `<dd>${book_info.illustrator.value}</dd>`
+				+ `<dt>Jazyková úprava</dt>`
+				+ `<dd>${book_info.language_editor.value}</dd>`
+				+ `<dt>Jazyk</dt>`
+				+ `<dd>${book_info.language.value}</dd>`
+				+ `<dt>ISBN</dt>`
+				+ `<dd>${book_info.isbn.value}</dd>`
+				+ `<dt>Rok vydania</dt>`
+				+ `<dd>${book_info.date_published.value.getFullYear()}</dd>`
+				+ `<dt>Vydavateľ</dt>`
+				+ `<dd>${book_info.publisher.value}</dd>`
+				+ `<dt>Vydanie</dt>`
+				+ `<dd>${book_info.book_edition.value}</dd>`
+				+ `<dt>Väzba</dt>`
+				+ `<dd>${book_info.book_format.value}</dd>`
+				+ `<dt>Rozmer</dt>`
+				+ `<dd>${book_info.dimensions.value}</dd>`
+				+ `<dt>Hmotnosť</dt>`
+				+ `<dd>${book_info.weight.value} g</dd>`
+				+ `<dt>Počet strán</dt>`
+				+ `<dd>${book_info.number_of_pages.value}</dd>`
+				+ `<dt>Písmo</dt>`
+				+ `<dd>${book_info.typeface.value}</dd>`
+				+ `<dt>Papier</dt>`
+				+ `<dd>${book_info.paper.value}</dd>`
+				+ `<dt>Tlač</dt>`
+				+ `<dd>${book_info.printer.value}</dd>`
+				+ `<dt>Finančná podpora</dt>`
+				+ `<dd>${book_info.financial_aid.value}</dd>`
 			+ `</dl>`
 		})
 	}
--- models/TheatrePlayInfo.js
+++ models/TheatrePlayInfo.js
@@ -9,34 +9,34 @@ export class TheatrePlayInfo extends ContentItem {
 		super({
 			richTextResolver: theatre_play_info =>
 				`<h2>Informácie o predstavení</h2>`
-			+ `<dl class="cp--book-info">`
-				+ `<dt class="cp--book-info__term">Názov</dt>`
-				+ `<dd class="cp--book-info__data">${theatre_play_info.title.value}</dd>`
-				+ `<dt class="cp--book-info__term">Premiéra</dt>`
-				+ `<dd class="cp--book-info__data">${moment(theatre_play_info.premiere_date.value).locale(dateLocale).format(dateFormat)}, ${theatre_play_info.premiere_place.value}</dd>`
-				+ `<dt class="cp--book-info__term">Scenár</dt>`
-				+ `<dd class="cp--book-info__data">${theatre_play_info.playwright.value}</dd>`
-				+ `<dt class="cp--book-info__term">Režisér</dt>`
-				+ `<dd class="cp--book-info__data">${theatre_play_info.director.value}</dd>`
-				+ `<dt class="cp--book-info__term">Hudba</dt>`
-				+ `<dd class="cp--book-info__data">${theatre_play_info.music.value}</dd>`
+			+ `<dl class="cp--dl">`
+				+ `<dt>Názov</dt>`
+				+ `<dd>${theatre_play_info.title.value}</dd>`
+				+ `<dt>Premiéra</dt>`
+				+ `<dd>${moment(theatre_play_info.premiere_date.value).locale(dateLocale).format(dateFormat)}, ${theatre_play_info.premiere_place.value}</dd>`
+				+ `<dt>Scenár</dt>`
+				+ `<dd>${theatre_play_info.playwright.value}</dd>`
+				+ `<dt>Režisér</dt>`
+				+ `<dd>${theatre_play_info.director.value}</dd>`
+				+ `<dt>Hudba</dt>`
+				+ `<dd>${theatre_play_info.music.value}</dd>`
 				+ (theatre_play_info.author_of_songs.value &&
-						(`<dt class="cp--book-info__term">Autor skladieb</dt>`
-					 + `<dd class="cp--book-info__data">${theatre_play_info.author_of_songs.value}</dd>`))
+						(`<dt>Autor skladieb</dt>`
+					 + `<dd>${theatre_play_info.author_of_songs.value}</dd>`))
 				+ (theatre_play_info.choreography.value &&
-						(`<dt class="cp--book-info__term">Choreografia</dt>`
-					 + `<dd class="cp--book-info__data">${theatre_play_info.choreography.value}</dd>`))
+						(`<dt>Choreografia</dt>`
+					 + `<dd>${theatre_play_info.choreography.value}</dd>`))
 				+ (theatre_play_info.dancing_group.value &&
-						(`<dt class="cp--book-info__term">Tanečníci</dt>`
-					 + `<dd class="cp--book-info__data">${theatre_play_info.dancing_group.value}</dd>`))
-				+ `<dt class="cp--book-info__term">Scénografia</dt>`
-				+ `<dd class="cp--book-info__data">${theatre_play_info.scenography.value}</dd>`
-				+ `<dt class="cp--book-info__term">Grafický dizajn</dt>`
-				+ `<dd class="cp--book-info__data">${theatre_play_info.graphic_design.value}</dd>`
-				+ `<dt class="cp--book-info__term">Fotografia</dt>`
-				+ `<dd class="cp--book-info__data">${theatre_play_info.photography.value}</dd>`
-				+ `<dt class="cp--book-info__term">Finančná podpora</dt>`
-				+ `<dd class="cp--book-info__data">${theatre_play_info.financial_aid.value}</dd>`
+						(`<dt>Tanečníci</dt>`
+					 + `<dd>${theatre_play_info.dancing_group.value}</dd>`))
+				+ `<dt>Scénografia</dt>`
+				+ `<dd>${theatre_play_info.scenography.value}</dd>`
+				+ `<dt>Grafický dizajn</dt>`
+				+ `<dd>${theatre_play_info.graphic_design.value}</dd>`
+				+ `<dt>Fotografia</dt>`
+				+ `<dd>${theatre_play_info.photography.value}</dd>`
+				+ `<dt>Finančná podpora</dt>`
+				+ `<dd>${theatre_play_info.financial_aid.value}</dd>`
 			+ `</dl>`
 		})
 	}
