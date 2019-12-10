b1421c0ffeacbc2e6bce92ce57c3e31897a1f486
Brano <surfin.zap@gmail.com>
2019-12-10 14:08:17 +0100

    Update project-list styles

--- components/projects-list.vue
+++ components/projects-list.vue
@@ -2,11 +2,13 @@
 	.container.container--project-list
 		ul.project-list
 			li.project-tile(v-for='project in projects' v-bind:key='project.meta__canonical_url')
+
 				a.project-tile__link(v-if='project.external_url' :href='project.meta__canonical_url' target='_blank')
 					.project-tile__credentials(:style='"background-color:" + project.color + ";"')
 						h2.project-tile__title {{ project.title }}
 						p.project-tile__description {{ project.short_description }}
 					img.project-tile__thumbnail(:src='project.image' :alt='project.image_description')
+
 				nuxt-link.project-tile__link(v-else :to='"projekt/" + project.meta__canonical_url')
 					.project-tile__credentials(:style='"background-color:" + project.color + ";"')
 						h2.project-tile__title {{ project.title }}
@@ -34,7 +36,6 @@
 		justify-items: stretch;
 		align-items: start;
 
-
 		@media (min-width: $screen-sm-min) {
 			grid-template-columns: 1fr 1fr;
 		}
@@ -46,6 +47,20 @@
 		}
 	}
 
+	.project-tile {
+		&:hover {
+			.project-tile__credentials {
+				animation: 350ms ease-out move-down forwards;
+
+				@keyframes move-down {
+					100% {
+						bottom: 0;
+					}
+				}
+			}
+		}
+	}
+
 	.project-tile__link {
 		text-decoration: none;
 		color: $white;
@@ -53,33 +68,26 @@
 		display: block;
 	}
 
-	.project-tile__thumbnail {
-		filter: opacity(30%);
-		width: calc(100% - 32px);
-		height: calc(100% - 32px);
-		margin: $grid-gap-column;
-
-		&:hover {
-			animation: 350ms ease-out desaturate forwards;
-
-			@keyframes desaturate {
-				100% {
-					filter: opacity(100%);
-					width: 100%;
-					height: 100%;
-					margin: 0;
-				}
-			}
-		}
-	}
 
 	.project-tile__credentials {
 		padding: $grid-gap-column;
 		position: absolute;
 		z-index: 1;
 		width: 100%;
-		top: 38%;
+		bottom: 37%;
 
+		@media (min-width: $screen-sm-min) {
+			bottom: 28%;
+		}
+		@media (min-width: $screen-md-min) {
+			bottom: 33%;
+		}
+		@media (min-width: $screen-lg-min) {
+			bottom: 27%;
+		}
+		@media (min-width: $screen-xl-min) {
+			bottom: 30%;
+		}
 	}
 
 	.project-tile__title {
@@ -87,8 +95,5 @@
 		font-weight: 800;
 		font-style: italic;
 		letter-spacing: 0.025em;
-
 	}
-
-
 </style>
