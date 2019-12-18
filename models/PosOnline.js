601560298650aad3ae50f6c2742f4576d7ef890d
Vít Svoboda <vit.svoboda@gmail.com>
2019-12-18 15:25:55 +0100

    Fix rendering of online pos

--- models/PosOnline.js
+++ models/PosOnline.js
@@ -3,23 +3,21 @@ import { ContentItem } from '@kentico/kontent-delivery';
 export class PosOnline extends ContentItem {
 	constructor() {
 		super({
-			richTextResolver: pos_online =>
-				`<div class="cp--pos-item `
-				+ (pos_online.primary_pos.value[0].codename == "yes" &&
-					(` cp--pos-item--primary`)) + `">`
+			richTextResolver: pos_online => {
+				const isPrimary = pos_online.primary_pos.value.length > 0 && pos_online.primary_pos.value[0].codename == "yes";
+				return `<div class="cp--pos-item ${isPrimary && ` cp--pos-item--primary`}">`
 				+ `${pos_online.pos.value}`
 				+ `<div class="cp--pos-item__action">`
-					+ `<a class="btn btn--block`
-						+ (pos_online.primary_pos.value[0].codename == "yes" &&
-							(` btn--cta`)) + `"`
+					+ `<a class="btn btn--block ${isPrimary && ` btn--cta`}"`
 						+ `href="${pos_online.url.value}`
 						+ `&utm_source=${pos_online.utm_source.value}`
 						+ `&utm_medium=${pos_online.utm_medium.value}`
 						+ `&utm_campaign=${pos_online.utm_campaign.value}"`
 						+ ` target="_blank">`
 						+ `${pos_online.url_label.value}</a>`
+					+ `</div>`
 				+ `</div>`
-			+ `</div>`
+			}
 		})
 	}
 }
