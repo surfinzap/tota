6d6e4f6a14b91d1e6b7694e8bf54206a7baff157
surfinzap <surfinzap@users.noreply.github.com>
2019-12-23 11:20:29 +0100

    Update to ternary conditions

--- models/PosOffline.js
+++ models/PosOffline.js
@@ -10,7 +10,7 @@ export class PosOffline extends ContentItem {
 								+ `<h4 class="cp--pos-item__title">${pos_offline.city.value}</h4>`
 								+ `<div class="cp--pos-item__subtitle">${pos_offline.title.value}</div>`
 								+ `<div class="cp--pos-item__address">${pos_offline.address.value}</div>`
-								+ `${hasMapURL && `<div class="cp--pos-item__map_url"><a href="${pos_offline.map_url.value}" target="_blank" rel="nofollow">→ otvoriť mapu</a></div>`}`
+								+ `${hasMapURL ? `<div class="cp--pos-item__map_url"><a href="${pos_offline.map_url.value}" target="_blank" rel="nofollow">→ otvoriť mapu</a></div>` : ``}`
 								+ `<div class="cp--pos-item__opening-hours-title">Otváracie hodiny</div>`
 								+ `<div class="cp--pos-item__opening-hours">${pos_offline.opening_hours.value}</div>`
 							+ `</div>`
--- models/PosOnline.js
+++ models/PosOnline.js
@@ -5,10 +5,10 @@ export class PosOnline extends ContentItem {
 		super({
 			richTextResolver: pos_online => {
 				const isPrimary = pos_online.primary_pos.value.length > 0 && pos_online.primary_pos.value[0].codename == "yes";
-				return `<div class="cp--pos-item ${isPrimary && ` cp--pos-item--primary`}">`
+				return `<div class="cp--pos-item ${isPrimary ? ` cp--pos-item--primary` : ``}">`
 				+ `${pos_online.pos.value}`
 				+ `<div class="cp--pos-item__action">`
-					+ `<a class="btn btn--block ${isPrimary && ` btn--cta`}"`
+					+ `<a class="btn btn--block ${isPrimary ? ` btn--cta` : ``}"`
 						+ `href="${pos_online.url.value}`
 						+ `&utm_source=${pos_online.utm_source.value}`
 						+ `&utm_medium=${pos_online.utm_medium.value}`
