5978d1000dcccce51337c0452e938b96e10e791f
surfinzap <surfinzap@users.noreply.github.com>
2019-12-28 16:05:45 +0100

    Make title optional for BiPoem

--- models/BiPoem.js
+++ models/BiPoem.js
@@ -6,12 +6,14 @@ export class BiPoem extends ContentItem {
 			richTextResolver: bi_poem =>
 				`<div class="cp--bi-poem">`
 					+ `<div class="cp--bi-poem__item">`
-					+ `<h2 class="cp--bi-poem__title">${bi_poem.title_lat.value}</h4>`
-					+ `<div class="cp--bi-poem__stanzas">${bi_poem.stanzas_lat.value}</div>`
-				+ `</div>`
+						+ (bi_poem.title_lat.value &&
+								(`<h2 class="cp--bi-poem__title">${bi_poem.title_lat.value}</h2>`))
+						+ `<div class="cp--bi-poem__stanzas">${bi_poem.stanzas_lat.value}</div>`
+					+ `</div>`
 					+ `<div class="cp--bi-poem__item">`
-					+ `<h2 class="cp--bi-poem__title">${bi_poem.title_rue.value}</h4>`
-					+ `<div class="cp--bi-poem__stanzas">${bi_poem.stanzas_rue.value}</div>`
+						+ (bi_poem.title_rue.value &&
+								(`<h2 class="cp--bi-poem__title">${bi_poem.title_rue.value}</h2>`))
+						+ `<div class="cp--bi-poem__stanzas">${bi_poem.stanzas_rue.value}</div>`
 				+ `</div>`
 			+ `</div>`
 		})
