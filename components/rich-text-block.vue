e3211d752d641f9bb98462b30463433d76a58c82
Vít Svoboda <vit.svoboda@gmail.com>
2019-12-28 14:22:40 +0100

    Refactor rich text element to a functional component
    This way, we don't need to mess around with templates that required to be wrapped in a fragment.

--- components/rich-text-block.vue
+++ components/rich-text-block.vue
@@ -1,42 +1,14 @@
 <template lang="pug">
-	mixin richTextElement
-		template(v-if='block.type === "P"')
-			p
-				block
-		template(v-else-if='block.type === "STRONG"')
-			strong
-				block
-		template(v-else-if='block.type === "#text"')
-			| {{block.value}}
-		template(v-else-if='block.type === "H1"')
-			h1
-				block
-		template(v-else-if='block.type === "H2"')
-			h2
-				block
-		template(v-else-if='block.type === "H3"')
-			h3
-				block
-		template(v-else-if='block.type === "H4"')
-			h4
-				block
-		template(v-else-if='block.type === "A"')
-			a(:href='block.data["href"]')
-				block
-		template(v-else)
-			// TODO here come the custom rich text renderer components
-
-	fragment
-		+richTextElement
+	rich-text-element(:block='block')
 			rich-text-block(v-for='(child, index) in block.children' :block='child' :key='index')
 </template>
 
 
 <script>
-	import { Fragment } from 'vue-fragment';
+	import RichTextElement from "./rich-text-element";
 	export default {
 		name: 'rich-text-block',
 		props: ['block'],
-		components: { Fragment },
+		components: {RichTextElement },
 	}
 </script>
---
+++ components/rich-text-element.vue
@@ -0,0 +1,19 @@
+<script>
+	export default {
+		name: 'rich-text-element',
+		functional: true,
+		props: ['block'],
+		render: (createElement, context) => {
+			const { block } = context.props;
+			switch (block.type) {
+				case 'OBJECT': {
+					return createElement('div', {}, [ context._v(block.data['data-type'])]);
+				}
+				case '#text':
+					return context._v(block.value);
+				default:
+					return createElement(block.type, {domProps: {...block.data}}, context.children);
+			}
+		}
+	}
+</script>
