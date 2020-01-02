46f701a3152d7445415636b70ea593307067dfb0
Vít Svoboda <vit.svoboda@gmail.com>
2020-01-02 19:58:45 +0100

    Fix extra divs introduced by components
    Though I'd suggest most of the headings go to the parent rich text element. They are content that the editor may want to change without messing with the client app.

--- components/linked-items/pos-list.vue
+++ components/linked-items/pos-list.vue
@@ -1,9 +1,6 @@
 <template lang="pug">
-	// TODO This wrapper is not present in the original. Do we want to ressurect the vue-fragment package?
-	div
-		h3 {{item.title.value}}
-		.cp--pos-list
-			rich-text(:content='item.resellers_list.value' :linkedItemComponent='linkedItemComponent')
+	.cp--pos-list
+		rich-text(:content='item.resellers_list.value' :linkedItemComponent='linkedItemComponent')
 </template>
 
 <script>
---
+++ components/linked-items/theatre-play-info-with-heading.vue
@@ -0,0 +1,15 @@
+<script>
+	import TheatrePlayInfo from './theatre-play-info';
+
+	export default {
+		functional: true,
+		props: ['item'],
+		render: (createElement, context) => {
+			const {props} = context;
+			return [
+				createElement('h2', 'Informácie o predstavení'),
+				createElement(TheatrePlayInfo, { props })
+			]
+		}
+	}
+</script>
