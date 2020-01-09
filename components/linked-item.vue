939994776efb2609419619935282f14e445852a5
Brano <surfin.zap@gmail.com>
2020-01-09 15:58:41 +0100

    Add headings directly to components

--- components/linked-item.vue
+++ components/linked-item.vue
@@ -6,25 +6,25 @@
 	import PosCz from './linked-items/pos-cz';
 	import PosOnline from './linked-items/pos-online';
 	import PosOffline from './linked-items/pos-offline';
-	import PosListWithHeading from './linked-items/pos-list-with-heading';
+	import PosList from './linked-items/pos-list';
 	import DefinitionList from './linked-items/definition-list';
 	import DefinitionTerm from './linked-items/definition-term';
-	import BookInfoWithHeading from './linked-items/book-info-with-heading';
+	import BookInfo from './linked-items/book-info';
 	import YoutubeVideo  from './linked-items/youtube-video';
 	import PullQuote from './linked-items/pull-quote';
 	import BiPoem from './linked-items/bi-poem';
-	import TheatrePlayInfoWithHeading from './linked-items/theatre-play-info-with-heading';
+	import TheatrePlayInfo from './linked-items/theatre-play-info';
 	import ProcurementItem from './linked-items/procurement-item';
 
 	const resolvers = {
 		'youtube_video': YoutubeVideo,
-		'book_info': BookInfoWithHeading,
-		'theatre_play_info': TheatrePlayInfoWithHeading,
+		'book_info': BookInfo,
+		'theatre_play_info': TheatrePlayInfo,
 		'pull_quote': PullQuote,
 		'figure_image': FigureImage,
 		'definition_list': DefinitionList,
 		'definition_term': DefinitionTerm,
-		'pos_list': PosListWithHeading,
+		'pos_list': PosList,
 		'pos_online': PosOnline,
 		'pos_offline': PosOffline,
 		'pos_cz': PosCz,
