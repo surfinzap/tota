b44d8f7f9ffd42144052f2f2587dfc49d46356e7
surfinzap <surfinzap@users.noreply.github.com>
2022-11-20 17:18:30 +0100

    📈 Add Simple Analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,19 @@
 <template lang="pug">
-	div
-		header-tota
-		.content-wrapper
-			nuxt
-			footer-tota
-		<!-- let's try postpone loading fonts -->
-		link(
-			rel='stylesheet',
-			href='https://fonts.googleapis.com/css?family=Alegreya:500,500i,700,800i,900i&display=swap&subset=cyrillic-ext,latin-ext'
-		)
+  div
+    header-tota
+    .content-wrapper
+      nuxt
+      footer-tota
+    <!-- let's try postpone loading fonts -->
+    link(
+      rel='stylesheet',
+      href='https://fonts.googleapis.com/css?family=Alegreya:500,500i,700,800i,900i&display=swap&subset=cyrillic-ext,latin-ext'
+    )
+    script(async='' 
+           defer='' 
+           :data-collect-dnt='devEnv'
+           :data-hostname='devEnv ? "localhost" : "tota.sk"'
+           src='https://scripts.simpleanalyticscdn.com/latest.js')
 </template>
 
 
@@ -17,34 +22,34 @@ import HeaderTota from '~/components/header-tota.vue'
 import FooterTota from '~/components/footer-tota.vue'
 
 export default {
-	components: {
-		HeaderTota,
-		FooterTota,
-	},
-	head: {
-		meta: [
-			{ name: 'google-site-verification', content: 'fx0sZBSOYt6hdMsZvKNXTVQ0YDt21sMVEGe2xMwBmF0' },
-		]
-	}
+  components: {
+    HeaderTota,
+    FooterTota,
+  },
+  computed: {
+    devEnv () {
+      return process.env.NODE_ENV === "development"
+    }
+  },
 }
 </script>
 
 <style lang='scss'>
-	.content-wrapper {
-		padding-top: $header-height--xs;
-		display: grid;
-		grid-template-rows: 1fr auto;
-		min-height: 100vh;
+  .content-wrapper {
+    padding-top: $header-height--xs;
+    display: grid;
+    grid-template-rows: 1fr auto;
+    min-height: 100vh;
 
-		@media (min-width: $screen-xs-min) {
-			padding-top: $header-height;
-		}
-	}
+    @media (min-width: $screen-xs-min) {
+      padding-top: $header-height;
+    }
+  }
 
 
-	.cc-banner {
-		font-family: $font-family--base;
-		font-size: $font-size--400;
-		line-height: $line-height--500;
-	}
+  .cc-banner {
+    font-family: $font-family--base;
+    font-size: $font-size--400;
+    line-height: $line-height--500;
+  }
 </style>
